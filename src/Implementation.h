1a4e66730d8047c172cb08e6518441988bac9951
Luís Fiolhais <luis.azenhas.fiolhais@tecnico.ulisboa.pt>
2019-03-29 10:38:26 +0000

    Increase MAX_DIGEST_BUFFER
    This is done to meet the same values in the open source tpm2-tss

--- src/Implementation.h
+++ src/Implementation.h
@@ -213,7 +213,7 @@
 #define  NUM_POLICY_PCR_GROUP           1
 #define  NUM_AUTHVALUE_PCR_GROUP        1
 #define  MAX_CONTEXT_SIZE               63176704
-#define  MAX_DIGEST_BUFFER              1024
+#define  MAX_DIGEST_BUFFER              2048
 #define  MAX_NV_INDEX_SIZE              2048
 #define  MAX_NV_BUFFER_SIZE             1024
 #define  MAX_CAP_BUFFER                 1024
